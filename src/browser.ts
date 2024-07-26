f02bc62b3c304a56f7c73bda74e00fd58c12e872
Nicolas Melo <nicolasmelo12@gmail.com>
2024-07-26 19:48:36 -0300

    hopefully make it work

--- src/browser.ts
+++ src/browser.ts
@@ -12,12 +12,11 @@ const startBrowserInstanceAndGetStories = async () => {
 
   const recursivelyGetStories = async (client: Awaited<ReturnType<typeof instagram>>) => {
     try {
-      const existsFile = fs.existsSync('stories.json')
-      const existingData = existsFile ? JSON.parse(fs.readFileSync('stories.json')?.toString() || '[]') : [];
-      const stories = await client.extractStories(existingData);
-      console.log('Stories', stories);
-      const newStories: Set<string> = new Set([...existingData, ...Array.isArray(stories) ? stories: typeof stories === 'string' ? [stories] : []]);
-      fs.writeFileSync('stories.json', JSON.stringify(Array.from(newStories), null, 2));
+      //const existsFile = fs.existsSync('stories.json')
+      //const existingData = existsFile ? JSON.parse(fs.readFileSync('stories.json')?.toString() || '[]') : [];
+      const stories = await client.extractStories([]);
+      //console.log('Stories', stories);
+      //fs.writeFileSync('stories.json', JSON.stringify(Array.from(stories), null, 2));
       setTimeout(() => recursivelyGetStories(client), 2000);
     } catch (e) {
       console.error('Error getting stories', e);
